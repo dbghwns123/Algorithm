a25e4531d5a420d3875dcb7f08f326ca4d2b9cf7
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-05-19 03:04:54 +0900

    [Bronze II] Title: OX퀴즈, Time: 144 ms, Memory: 15952 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int test_cnt = Integer.parseInt(br.readLine());
+    for (int i = 0; i < test_cnt; i++) {
+      String s = br.readLine();
+      int cnt = 0;
+      int sum = 0;
+      for (int j = 0; j < s.length(); j++) {
+        if (s.charAt(j) == 'O') {
+          cnt++;
+          sum += cnt;
+        } else {
+          cnt = 0;
+        }
+      }
+      sb.append(sum + "\n");
+    }
+    System.out.println(sb);
+  }
+
+}
