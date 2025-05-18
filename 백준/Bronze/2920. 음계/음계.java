6412d70df3aa1989a6815c53eac5977ce68a19ec
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-05-19 01:45:00 +0900

    [Bronze II] Title: 음계, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    int[] arr = new int[8];
+    for (int i = 0; i < arr.length; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+
+    String s = "";
+    for (int i = 0; i < arr.length - 1; i++) {
+      if (arr[i + 1] == arr[i] + 1) {
+        s = "ascending";
+      } else if (arr[i + 1] == arr[i] - 1) {
+        s = "descending";
+      } else {
+        s = "mixed";
+        break;
+      }
+    }
+    System.out.println(s);
+  }
+
+}
