0029b60fdf5cc71c4f0fded02d686e3965d9168c
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-05-18 19:05:08 +0900

    [Bronze III] Title: 알람 시계, Time: 132 ms, Memory: 16136 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -9,9 +9,22 @@ public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-    LocalTime localTime = LocalTime.of(Integer.parseInt(st.nextToken()),
-        Integer.parseInt(st.nextToken())).minusMinutes(45);
-    System.out.println(localTime.getHour() + " " + localTime.getMinute());
+//    LocalTime localTime = LocalTime.of(Integer.parseInt(st.nextToken()),
+//        Integer.parseInt(st.nextToken())).minusMinutes(45);
+//    System.out.println(localTime.getHour() + " " + localTime.getMinute());
+    int hour = Integer.parseInt(st.nextToken());
+    int minute = Integer.parseInt(st.nextToken());
+    if (minute < 45) {
+      if (hour == 0) {
+        hour = 23;
+      } else {
+        hour--;
+      }
+      minute = 60 - (45 - minute);
+    } else {
+      minute -= 45;
+    }
+    System.out.println(hour + " " + minute);
   }
 
 }
