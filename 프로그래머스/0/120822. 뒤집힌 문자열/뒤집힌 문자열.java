6c9bb24cde0e37936cdd7a894b07be7991c01067
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-08-04 14:48:17 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 2.17 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        String[] split = my_string.split("");
+        for (int i = split.length - 1; i >= 0; i--) {
+          answer += split[i];
+        }
+        return answer;
+    }
+}
