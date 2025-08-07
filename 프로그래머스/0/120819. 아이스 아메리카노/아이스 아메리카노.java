eb69a0ea067e00d544acbdf9134f903811d408d7
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-08-07 12:36:02 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(int money) {
+        int[] arr = new int[2];
+
+        arr[0] = money / 5500;
+        arr[1] = money % 5500;
+        return arr;
+    }
+}
