f1fc55a9ad4f971248f5d887538d396670d49d57
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-08-08 11:37:17 +0900

    [level 0] Title: 모음 제거, Time: 0.22 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string) {
+        return my_string.replaceAll("a", "")
+        .replaceAll("e", "")
+        .replaceAll("i", "")
+        .replaceAll("o", "")
+        .replaceAll("u", "");
+    }
+}
