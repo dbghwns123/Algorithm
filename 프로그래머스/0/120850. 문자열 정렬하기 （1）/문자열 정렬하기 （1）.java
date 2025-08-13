de58d9395065b4555e4a8af105af2e23ffb73911
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-08-13 17:09:50 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 1.37 ms, Memory: 86.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String my_string) {
+        my_string = my_string.replaceAll("[a-z]", "");
+        int[] answer = new int[my_string.length()];
+        for (int i = 0; i < my_string.length(); i++) {
+          answer[i] = Integer.parseInt(my_string.substring(i, i + 1));
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
