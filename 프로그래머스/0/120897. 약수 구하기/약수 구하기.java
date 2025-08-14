4b2b0b158e69e2b7be37aef809907fa9207b68ae
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-08-14 16:24:17 +0900

    [level 0] Title: 약수 구하기, Time: 5.21 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+class Solution {
+    public int[] solution(int n) {
+        return IntStream.rangeClosed(1, n).filter(v -> n % v == 0).toArray();
+    }
+}
