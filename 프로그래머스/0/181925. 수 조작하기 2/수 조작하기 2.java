06f093fbc84497c5aba06f3755c3c1a886197902
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-08-18 15:21:50 +0900

    [level 0] Title: 수 조작하기 2, Time: 26.46 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(int[] numLog) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < numLog.length; i++) {
+            int result = numLog[i] - numLog[i - 1];
+            if (result == 1) {
+                sb.append("w");
+            } else if (result == -1) {
+                sb.append("s");
+            } else if (result == 10) {
+                sb.append("d");
+            } else {
+                sb.append("a");
+            }
+        }
+        return sb.toString();
+    }
+}
