594590c87725bf5b1f3b09cdbf29a148cbb9bb70
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-08-18 17:22:18 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,13 +1,6 @@
 class Solution {
     public int solution(String myString, String pat) {
-        String answer = "";
-        for (String s : myString.split("")) {
-          if (s.equals("A")) {
-            answer += "B";
-          } else {
-            answer += "A";
-          } 
-        }
-        return answer.contains(pat) ? 1 : 0;
+        myString = myString.replace("A", "a").replace("B", "A").replace("a", "B");
+        return myString.contains(pat) ? 1 : 0;
     }
 }
