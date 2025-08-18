659ed7c4241fa4d6a88d5f2b760aef5843e928e0
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-08-18 17:15:21 +0900

    [level 0] Title: 숫자 찾기, Time: 8.74 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,11 +1,5 @@
 class Solution {
     public int solution(int num, int k) {
-        String[] split = String.valueOf(num).split("");
-        for (int i = 0; i < split.length; i++) {
-          if (split[i].equals(String.valueOf(k))) {
-            return i + 1;
-          }
-        }
-        return -1;
+        return ("-" + num).indexOf(String.valueOf(k));
     }
 }
