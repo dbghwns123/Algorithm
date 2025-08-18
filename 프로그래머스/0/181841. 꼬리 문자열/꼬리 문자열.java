644912020c05332c848d20cf576c8dbcf4dc7472
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-08-18 20:20:28 +0900

    [level 0] Title: 꼬리 문자열, Time: 2.54 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
@@ -1,11 +1,7 @@
+import java.util.*;
+import java.util.stream.Collectors;
 class Solution {
     public String solution(String[] str_list, String ex) {
-        String answer = "";
-        for (String s : str_list) {
-            if (!s.contains(ex)) {
-                answer += s;
-            }
-        }
-        return answer;
+        return Arrays.stream(str_list).filter(s -> !s.contains(ex)).collect(Collectors.joining());
     }
 }
