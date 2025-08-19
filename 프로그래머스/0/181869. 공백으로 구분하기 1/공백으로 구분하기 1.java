a25710242cd9e7f062473337c578a00017396ff0
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-08-19 22:16:02 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.23 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -4,10 +4,6 @@ import java.util.List;
 
 class Solution {
     public String[] solution(String my_string) {
-        List<String> answer = new ArrayList<>();
-        for (String s : my_string.split(" ")) {
-            answer.add(s);
-        }
-        return answer.toArray(new String[answer.size()]);
+        return my_string.split(" ");
     }
 }
