411a9d7d1b4fa963d0edbe2ac7f95ddf97db247a
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-08-20 17:47:18 +0900

    [level 0] Title: 할 일 목록, Time: 13.45 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -1,12 +1,11 @@
 import java.util.*;
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
-        List<String> list = new ArrayList<>();
-    for (int i = 0; i < finished.length; i++) {
-      if (!finished[i]) {
-        list.add(todo_list[i]);
-      }
-    }
-    return list.toArray(new String[0]);
+        String str = "";
+        for(int i=0; i<finished.length; i++){
+            str = finished[i]==false ? str+todo_list[i]+"," : str;
+        }
+
+        return str.split(",");
     }
 }
