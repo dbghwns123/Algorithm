6d816f07d356df330ac7287787164abad9026e7c
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-08-20 12:22:26 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.13 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
@@ -1,26 +1,14 @@
 class Solution {
     public int solution(String binomial) {
-        if (binomial.contains(" + ")) {
-            int index = binomial.indexOf(" + ");
-            String front = binomial.substring(0, index);
-            Integer front1 = Integer.valueOf(front);
-            String behind = binomial.substring(index + 3, binomial.length());
-            Integer behind1 = Integer.valueOf(behind);
-            return front1 + behind1;
-        } else if (binomial.contains(" - ")) {
-            int index = binomial.indexOf(" - ");
-            String front = binomial.substring(0, index);
-            Integer front1 = Integer.valueOf(front);
-            String behind = binomial.substring(index + 3, binomial.length());
-            Integer behind1 = Integer.valueOf(behind);
-            return front1 - behind1;
-        } else {
-            int index = binomial.indexOf(" * ");
-            String front = binomial.substring(0, index);
-            Integer front1 = Integer.valueOf(front);
-            String behind = binomial.substring(index + 3, binomial.length());
-            Integer behind1 = Integer.valueOf(behind);
-            return front1 * behind1;
-        }
+        String[] split = binomial.split(" ");
+    int a = Integer.parseInt(split[0]);
+    int b = Integer.parseInt(split[2]);
+    if (split[1].equals("+")) {
+      return a + b;
+    } else if (split[1].equals("-")) {
+      return a - b;
+    } else {
+      return a * b;
+    }
     }
 }
