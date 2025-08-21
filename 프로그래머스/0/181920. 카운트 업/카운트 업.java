855bc60e9977579fbbbf867ed937379e4a6ef579
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-08-21 15:24:25 +0900

    [level 0] Title: 카운트 업, Time: 10.26 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,17 +1,7 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = start_num; i <= end_num; i++) {
-            list.add(i);
-        }
-        int[] answer = new int[list.size()];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = list.get(i);
-        }
-        return answer;
+        return IntStream.rangeClosed(start_num, end_num).toArray();
     }
 }
