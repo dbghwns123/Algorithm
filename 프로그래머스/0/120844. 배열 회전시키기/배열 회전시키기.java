47df46ab991978feb9df6794701574d44dfd0564
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-09-13 02:13:29 +0900

    [level 0] Title: 배열 회전시키기, Time: 4.99 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -2,19 +2,14 @@ import java.util.*;
 import java.util.stream.*;
 class Solution {
     public int[] solution(int[] numbers, String direction) {
-        int[] answer = new int[numbers.length];
-        
-        if(direction.equals("right")){
-            for(int i = 0; i < answer.length - 1; i++){
-                answer[i + 1] = numbers[i];
-            }
-            answer[0] = numbers[numbers.length -1];
-        }else {
-            for(int i = 0; i < answer.length - 1; i++){
-                answer[i] = numbers[i + 1];
-            }
-            answer[answer.length - 1] = numbers[0];
-        }
-        return answer;
+        List<Integer> list = Arrays.stream(numbers).boxed().collect(Collectors.toList());
+    if (direction.equals("right")) {
+      Integer remove = list.remove(list.size() - 1);
+      list.add(0, remove);
+    } else {
+      Integer remove = list.remove(0);
+      list.add(remove);
+    }
+    return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
