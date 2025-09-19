47af659449bfb66840da2c26c59140ab28da90ac
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-09-20 00:11:50 +0900

    [level 0] Title: 369게임, Time: 0.19 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -2,12 +2,20 @@ import java.util.*;
 class Solution {
     public int solution(int order) {
         int answer = 0;
-        while(order > 0) {
-            if(order % 10 == 3 || order % 10 == 6 || order % 10 == 9) {
+        String str = String.valueOf(order);
+        for(String s : str.split("")) {
+            if(s.equals("3") || s.equals("6") || s.equals("9")) {
                 answer++;
             }
-            order /= 10;
         }
         return answer;
+        // int answer = 0;
+        // while(order > 0) {
+        //     if(order % 10 == 3 || order % 10 == 6 || order % 10 == 9) {
+        //         answer++;
+        //     }
+        //     order /= 10;
+        // }
+        // return answer;
     }
 }
