c6d87c9766a572094e862e4c6c55f5e5623d5b18
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-09-22 23:07:50 +0900

    [level 0] Title: 배열 만들기 5, Time: 20.03 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -1,13 +1,14 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] intStrs, int k, int s, int l) {
-        List<Integer> list = new ArrayList<>();
-        for(String s1 : intStrs) {
-            Integer i = Integer.parseInt(s1.substring(s, s + l));
-            if(i > k) {
-                list.add(i);
-            }
-        }
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        return Arrays.stream(intStrs).mapToInt(value -> Integer.parseInt(value.substring(s,s+l))).filter(v -> v > k).toArray();
+        // List<Integer> list = new ArrayList<>();
+        // for(String s1 : intStrs) {
+        //     Integer i = Integer.parseInt(s1.substring(s, s + l));
+        //     if(i > k) {
+        //         list.add(i);
+        //     }
+        // }
+        // return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
