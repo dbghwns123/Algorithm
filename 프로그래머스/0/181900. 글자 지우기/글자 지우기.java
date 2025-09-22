b6fc4f93097228ed2573ac76e01cf3f137e543f1
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-09-22 20:27:45 +0900

    [level 0] Title: 글자 지우기, Time: 0.42 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,13 +1,18 @@
 import java.util.*;
 class Solution {
     public String solution(String my_string, int[] indices) {
-        StringBuilder sb = new StringBuilder(my_string);
-        int a = 0;
-        Arrays.sort(indices);
-        for (int index : indices) {
-          sb.deleteCharAt(index - a);
-          a++;  
+        String[] arr = my_string.split("");
+        for(int i : indices) {
+            arr[i] = "";
         }
-        return sb.toString();
+        return String.join("",arr);
+        // StringBuilder sb = new StringBuilder(my_string);
+        // int a = 0;
+        // Arrays.sort(indices);
+        // for (int index : indices) {
+        //   sb.deleteCharAt(index - a);
+        //   a++;  
+        // }
+        // return sb.toString();
     }
 }
