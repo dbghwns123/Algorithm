802cffffd899fd363051b912944be79c4b38b320
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-09-29 23:24:20 +0900

    [level 0] Title: k의 개수, Time: 5306.05 ms, Memory: 392 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -1,15 +1,10 @@
 class Solution {
     public int solution(int i, int j, int k) {
-        int answer = 0;
-        String strK = String.valueOf(k);
+        String str = "";
         for(int a = i; a <= j; a++) {
-            String str = String.valueOf(a);
-            if(str.contains(strK)) {
-                for(String s : str.split("")) {
-                    if(s.equals(strK)) answer++;
-                }
-            }
+            str += a+"";
         }
-        return answer;
+
+        return str.length() - str.replace(k+"", "").length();
     }
 }
