ecc64b0424cb302bc1ff1356b775c5f5d256d848
yu ho jun <156578961+dbghwns123@users.noreply.github.com>
2025-09-30 22:26:38 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int a = 1;
+        for(int i = 1; i <= 10; i++) {
+            a *= i;
+            if(a == n) {
+                return i;
+            } else if(a < n) {
+                answer = i;
+            } else {
+                return answer;
+            }
+        }
+        return answer;
+    }
+}
