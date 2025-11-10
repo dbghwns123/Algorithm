b673750c46daa8d5b8cfbd5dc28139c9dd5c5ccf
Hojun Yu <156578961+dbghwns123@users.noreply.github.com>
2025-11-11 00:26:06 +0900

    [level 1] Title: 폰켓몬, Time: 14.93 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,12 +1,19 @@
 import java.util.*;
+import java.util.stream.*;
 class Solution {
     public int solution(int[] nums) {
         // set을 사용하는 방법
-        Set<Integer> set = new HashSet<>();
-        for(int i : nums) {
-            set.add(i);
-        }
+//         Set<Integer> set = new HashSet<>();
+//         for(int i : nums) {
+//             set.add(i);
+//         }
         
-        return Math.min(set.size(), nums.length / 2);
+//         return Math.min(set.size(), nums.length / 2);
+        
+        // stream을 사용하는 방법
+        return Arrays.stream(nums)
+        .boxed()
+        .collect(Collectors.collectingAndThen(Collectors.toSet(),
+            phonekemons -> Integer.min(nums.length / 2, phonekemons.size())));
     }
 }
