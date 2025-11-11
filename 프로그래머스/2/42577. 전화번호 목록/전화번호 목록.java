0303675141719b11201c5ed91cb6905dbfebe1b0
Hojun Yu <156578961+dbghwns123@users.noreply.github.com>
2025-11-11 11:38:53 +0900

    [level 2] Title: 전화번호 목록, Time: 305.98 ms, Memory: 217 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,10 +2,24 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         // 정렬 + 인접 비교
-        Arrays.sort(phone_book);
-        for(int i = 0; i < phone_book.length - 1; i++) {
-            if(phone_book[i + 1].startsWith(phone_book[i])) {
-                return false;
+        // Arrays.sort(phone_book);
+        // for(int i = 0; i < phone_book.length - 1; i++) {
+        //     if(phone_book[i + 1].startsWith(phone_book[i])) {
+        //         return false;
+        //     }
+        // }
+        // return true;
+        
+        // HashSet 사용
+        Set<String> set = new HashSet<>();
+        for(String s : phone_book) {
+            set.add(s);
+        }
+        for(String s : phone_book) {
+            for(int i = 1; i < s.length(); i++) {
+                if(set.contains(s.substring(0, i))) {
+                    return false;
+                }
             }
         }
         return true;
