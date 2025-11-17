40a205db6d421a45f75647162a97800e21fef4fa
Hojun Yu <156578961+dbghwns123@users.noreply.github.com>
2025-11-17 12:43:04 +0900

    [level 2] Title: 올바른 괄호, Time: 55.96 ms, Memory: 60.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        Deque<String> stack = new ArrayDeque<>();
+        for(String s1 : s.split("")) {
+            if(s1.equals("(")) {
+                stack.addLast("(");
+            } else {
+                if(stack.isEmpty()) {
+                    return false;
+                }else {
+                    stack.pollLast();
+                }
+            }
+        }
+        return stack.isEmpty() ? true : false;
+    }
+}
