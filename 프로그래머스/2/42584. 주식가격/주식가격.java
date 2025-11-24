78d7cb3b17bcc6d30c262dd105df8473617f379d
Hojun Yu <156578961+dbghwns123@users.noreply.github.com>
2025-11-24 19:21:36 +0900

    [level 2] Title: 주식가격, Time: 23.98 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,17 +3,13 @@ class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
         for(int i = 0; i < prices.length; i++) {
-            int cur = prices[i];
-            int notLow = 0;
             for(int j = i + 1; j < prices.length; j++) {
-                notLow++;
-                if(prices[j] < cur) {
+                answer[i]++;
+                if(prices[j] < prices[i]) {
                     break;
                 }
             }
-            answer[i] = notLow;
         }
-        answer[answer.length - 1] = 0;
         return answer;
     }
 }
